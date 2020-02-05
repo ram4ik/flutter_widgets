1ffe017227463cb249cd9cd9f9eadb5c320b7a59
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-02-05 13:30:19 +0200

    with center text style

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Center(
-        child: Text(
-          "Welcome to Flutter",
-          style: TextStyle(fontSize: 30),
-          textDirection: TextDirection.ltr,
+    MaterialApp( // For iOS -> Cupertino
+      home: Scaffold(
+
+        backgroundColor: Colors.lightBlueAccent,
+
+        appBar: AppBar(
+          title: Text("My First App"),
+          centerTitle: true,
+        ),
+
+        body: Center(
+          child: Text("This is my home page",
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
         ),
       ),
     ),
   );
+
 }
